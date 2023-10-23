84e074abf8a827fc2b504e6fc9ba1a680f883906
Hotpotato7420 <axejak056@gmail.com>
2023-10-23 14:34:13 +0200

    Uppgifter 5.1-5.3

---
+++ src/Uppgift5_3.java
@@ -0,0 +1,9 @@
+public class Uppgift5_3 {
+    public static void main(String[] args) {
+        int years = 0;
+        for (double salary=0.01; salary<=10000000;salary *=2){
+            years++;
+        }
+        System.out.println(dagar);
+    }
+}
